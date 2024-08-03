31a379cc9238f5cd78a8a7ef32199b696439efb9
Muhammad Saleh Saqib <salehsaqib@gmail.com>
2024-08-03 00:01:31 -0700

    printing class objects

--- class1.py
+++ class1.py
@@ -2,14 +2,11 @@
 First class from a youtube tutorial
 """
 class Item:
-    pass
-
+    def calculate_total_price(self, x, y):
+        return x*y
+    
 item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
-
-print (type(item1))
-print (type(item1.name))
-print (type(item1.price))
-print (type(item1.quantity))
+print(item1.calculate_total_price(item1.price, item1.quantity))
